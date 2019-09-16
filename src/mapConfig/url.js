a12d4273445b18fffbb1540ae475840e33250219
LuckRain <763839938@qq.com>
2019-09-16 16:19:55 +0800

    Geojson update

--- src/mapConfig/url.js
+++ src/mapConfig/url.js
@@ -1,36 +1,36 @@
-const baseUrl = 'http://192.168.3.5:8080/';
+// const baseUrl = 'http://192.168.3.5:8080/';
 
 
 // 休闲娱乐
-export const relaxation_url = baseUrl + 'geoserver/test/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=test%3Axxyl&maxFeatures=50&outputFormat=application%2Fjson';
+export const relaxation_url = '/data/relaxation.json';
 export const relaxation_color = [70, 130, 180, 255];
 //景观
-export const scenery_url = baseUrl + 'geoserver/test/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=test%3Ajg&maxFeatures=50&outputFormat=application%2Fjson';
+export const scenery_url = '/data/scenery.json';
 export const scenery_color = [255, 140, 0, 255];
 //种植区
-export const planting_url = baseUrl + 'geoserver/test/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=test%3Azzq&maxFeatures=50&outputFormat=application%2Fjson';
+export const planting_url = '/data/planting.json';
 export const planting_color = [34, 139, 34, 255];
 //住宿
-export const accommodation_url = baseUrl + 'geoserver/test/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=test%3Azs&maxFeatures=50&outputFormat=application%2Fjson';
+export const accommodation_url = '/data/accommodation.json';
 export const accommodation_color = [128, 128, 128, 255];
 //生产车间
-export const producingDepartment_url = baseUrl + 'geoserver/test/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=test%3Asccj&maxFeatures=50&outputFormat=application%2Fjson';
+export const producingDepartment_url ='/data/producingDepartment.json';
 export const producingDepartment_color = [128, 128, 128, 255];
 //餐厅
-export const restaurant_url = baseUrl + 'geoserver/test/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=test%3Act&maxFeatures=50&outputFormat=application%2Fjson';
+export const restaurant_url = '/data/restaurant.json';
 export const restaurant_color = [255, 165, 0, 255];
 //葡萄
-export const grape_url = baseUrl + 'geoserver/test/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=test%3Apt&maxFeatures=50&outputFormat=application%2Fjson';
+export const grape_url = '/data/grape.json';
 export const grape_color = [138,43,226, 255];
 //服务设施
-export const server_url = baseUrl + 'geoserver/test/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=test%3Afw&maxFeatures=50&outputFormat=application%2Fjson';
+export const server_url = '/data/server.json';
 export const server_color = [0,139,139, 255];
 //大数据中心
-export const bigDataCenter_url = baseUrl + 'geoserver/test/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=test%3Adsjzx&maxFeatures=50&outputFormat=application%2Fjson';
+export const bigDataCenter_url = '/data/bigDataCenter.json';
 export const bigDataCenter_color = [220,20,60, 255];
 //草莓种植大棚
-export const strawberry_url = baseUrl + 'geoserver/test/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=test%3Acmzzdp&maxFeatures=50&outputFormat=application%2Fjson';
+export const strawberry_url = '/data/strawberry.json';
 export const strawberry_color = [255,182,193, 255];
 //智能温室
-export const intelligentGreenhouse_url = baseUrl + 'geoserver/test/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=test%3Aznws&maxFeatures=50&outputFormat=application%2Fjson';
+export const intelligentGreenhouse_url = '/data/intelligentGreenhouse.json';
 export const intelligentGreenhouse_color = [0,100,0, 255];
